107dd0ad759a4c5d171e54b474af18a6cee4c4e9
Mustafa <mkafridi073@gmail.com>
2022-07-23 19:38:38 +0500

    All done for Day-5

--- Day-7 Review/review.java
+++ Day-7 Review/review.java
@@ -7,11 +7,22 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        String array[] = new String(20);
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i<20; i++)
+        String inputString = new String(20);
+        Array StrArray = new Array(inputString.length());
+        Array StrEven = new Array(inputString.length()/2);
+        Array StrOdd = new Array(inputString.length()/2);
+        // Scanner for String input
+        Scanner inputScanner = new Scanner(System.in);
+        inputString = inputScanner.nextLine();
+        for(int i = 0; i<inputString.length(); i++)
         {
-            
+            StrArray[i] = inputString.charAt(i);
+        }
+        for(int i = 0; i<inputString.length(); i+2)
+        {
+            int j=0;
+            StrEven[j] = StrArray[i];
+            j++;
         }
     }
 }
