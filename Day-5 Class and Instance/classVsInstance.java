732e027e21ee56331b997e28b9f7d5a12ce55ee1
Mustafa <mkafridi073@gmail.com>
2022-07-23 12:55:23 +0500

    All done for Day-4

---
+++ Day-5 Class and Instance/classVsInstance.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+  		// Add some more code to run some checks on initialAge
+	}
+
+	public void amIOld() {
+  		// Write code determining if this person's age is old and print the correct statement:
+        System.out.println(/*Insert correct print statement here*/);
+	}
+
+	public void yearPasses() {
+  		// Increment this person's age.
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Person p = new Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+        }
+		sc.close();
+    }
+}
