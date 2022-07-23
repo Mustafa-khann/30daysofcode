3cd5b90498d50b80c3bdedba28de444823e20662
Mustafa <mkafridi073@gmail.com>
2022-07-23 19:13:09 +0500

    All done for Day-5

---
+++ Day-7 Review/review.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String array[] = new String(20);
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i<20; i++)
+        {
+            
+        }
+    }
+}
