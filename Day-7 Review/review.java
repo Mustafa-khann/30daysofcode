30e527788218aa1a16712052a0a09a616a39940b
Mustafa <mkafridi073@gmail.com>
2022-07-24 15:26:40 +0500

    All done for Day-7

--- Day-7 Review/review.java
+++ Day-7 Review/review.java
@@ -1,12 +1,28 @@
 import java.io.*;
-import org.apache.commons.lang3.StringUtils;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        
-    }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();sc.nextLine();
+        String[] array = new String[n];
+        for(int i = 0; i<n; i++)
+        {
+            array[i] = sc.nextLine();
+        }
+        for(int i = 0; i<n; i++)
+        {
+            char[] ch = array[i].toCharArray();
+            for(int e = 0;e<ch.length; e+=2)
+            {
+                System.out.print(ch[e]);
+            }
+            System.out.print(" ");
+            for(int o = 1; o<ch.length; o+=2)
+            {
+                System.out.print(ch[o]);
+            }
+            System.out.println("");
+        }
+    }}
