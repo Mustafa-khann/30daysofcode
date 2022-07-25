a47fd480fd9830455049db1644f38105815bb35b
Mustafa <mkafridi073@gmail.com>
2022-07-25 21:00:30 +0500

    All done for Day-9

--- Day-9 Dictionaries/githubpush.sh
+++ Day-9 Dictionaries/githubpush.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Shell script to automate the code pushing to github.
-While true
-sleep 120
-git add .
-git commit -a -m "All done for Day-9"
-git push
+while :
+do
+# DELAY OF 2 MINUTES FOR EVERY PUSH IN THE INFINITE LOOP
+    sleep 120
+# COMMANDS TO PUSH THE CODE TO GITHUB
+    git add .
+    git commit -a -m "All done for Day-9"
+    git push
+done
