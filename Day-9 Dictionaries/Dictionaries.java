26d3704ab73e8716bab6b37f92750beeb2f67ea9
Mustafa <mkafridi073@gmail.com>
2022-07-25 21:58:49 +0500

    All done for Day-9

--- Day-9 Dictionaries/Dictionaries.java
+++ Day-9 Dictionaries/Dictionaries.java
@@ -6,19 +6,22 @@ class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        HashMap<String,Integer> map = new HashMap<String,Integer>();
+        HashMap<String,Integer> hm = new HashMap<String,Integer>();
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
             // Write code here
-            map.put(name,phone);
+            hm.put(name,phone);
         }
         while(in.hasNext()){
             String s = in.next();
             // Write code here
-            if(map.containsKey(s))
+            if(hm.containsKey(s)){
+                System.out.println(s+"="+hm.get(s));
+            }
+            else
             {
-                System.out.print(s+"="+s.get(s));
+                System.out.println("Not found");
             }
         }
         in.close();
