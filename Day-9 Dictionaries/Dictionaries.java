34913f21ccc224ee7b0937bab7d4aad8b0914057
Mustafa <mkafridi073@gmail.com>
2022-07-25 21:44:54 +0500

    All done for Day-9

--- Day-9 Dictionaries/Dictionaries.java
+++ Day-9 Dictionaries/Dictionaries.java
@@ -6,14 +6,19 @@ class Solution{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
         for(int i = 0; i < n; i++){
             String name = in.next();
             int phone = in.nextInt();
             // Write code here
+            hm.put(name,phone);
         }
         while(in.hasNext()){
             String s = in.next();
             // Write code here
+            if(hm.containsKey(s)){
+                
+            }
         }
         in.close();
     }
--- Day-9 Dictionaries/githubpush.sh
+++ Day-9 Dictionaries/githubpush.sh
@@ -3,10 +3,10 @@
 # Shell script to automate the code pushing to github.
 while :
 do
-# DELAY OF 2 MINUTES FOR EVERY PUSH IN THE INFINITE LOOP
-    sleep 120
 # COMMANDS TO PUSH THE CODE TO GITHUB
     git add .
     git commit -a -m "All done for Day-9"
     git push
+# DELAY OF 2 MINUTES FOR EVERY PUSH IN THE INFINITE LOOP
+    sleep 120
 done
