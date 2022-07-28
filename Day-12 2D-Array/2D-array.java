fe45819f716f9afcd9d15de6195ed8c30236acdb
Mustafa <mkafridi073@gmail.com>
2022-07-28 21:48:52 +0500

    All done for Day-12

--- Day-12 2D-Array/2D-array.java
+++ Day-12 2D-Array/2D-array.java
@@ -32,7 +32,7 @@ public class Solution {
         {
             for(int j = 0; j<3; j++)
             {
-                int sum += (arr.get(i).get(j) + arr.get(i).get(j));
+                int sum += arr.get(i).get(j) + arr.get(i).get(j);
                 if(temp<sum)
                 {
                     temp = sum;
