2ebdf20b7146c558c40016b4a7e527e022e144e7
Mustafa <mkafridi073@gmail.com>
2022-07-29 22:44:31 +0500

    All done for Day-12

---
+++ Day-12 2D-Array/2D-array.java
@@ -0,0 +1,48 @@
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+        int max = 0;
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+        bufferedReader.close();
+         // Solution
+    for(int l = 0; l<6; l++)
+    {
+        for(int k=l; k<4; k++)
+        {
+            for(int i = k; i<3; i++)
+            {
+                for(int j = i; j<3; j++)
+                {
+                    System.out.println(arr.get(i).get(j));
+                }
+            }
+        }
+    }
+    }
+}
