b03ec1f779c21f92d6b8b9eb741b76d56799a65f
Mustafa <mkafridi073@gmail.com>
2022-08-03 10:51:44 +0500

    All done for Day-12

--- Day-12 2D-Array/2D-array.java
+++ Day-12 2D-Array/2D-array.java
@@ -12,28 +12,10 @@ import java.util.regex.*;
 
 
 public class Solution {
-    public static int hourGlassSum(ArrayList<Integer>)
-    {
-        int rows = arr.length;
-        int columns = arr[0].length;
-        int maxHourglassSum = -63;
-        for(int i = 0; i<rows-2; i++)
-        {
-            for(int j = 0; j<columns-2; j++)
-            {
-                int currentHourglassSum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1]
-                +arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
-                maxHourglassSum = Math.max(maxHourglassSum, currentHourglassSum);
-            }
-        }
-        return maxHourglassSum;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         List<List<Integer>> arr = new ArrayList<>();
-        int max = 0;
-        int sum = 0;
         for (int i = 0; i < 6; i++) {
             String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
@@ -47,7 +29,18 @@ public class Solution {
             arr.add(arrRowItems);
         }
         bufferedReader.close();
-        int output = hourGlassSum(arr);
-        System.out.println(output);
+        int rows = arr.size();
+        int columns = arr.get(0).size(); 
+        int maxHourglassSum = -63;
+        for(int i = 0; i<rows-2; i++)
+        {
+            for(int j = 0; j<columns-2; j++)
+            {
+                int currentHourglassSum = arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(i+2) + arr.get(i+1).get(j+1)
+                +arr.get(i+2).get(j)+arr.get(i+2).get(j+1)+arr.get(i+2).get(j+2);
+                maxHourglassSum = Math.max(maxHourglassSum, currentHourglassSum);
+            }
+        }
+        System.out.print(maxHourglassSum);
     }
 }
