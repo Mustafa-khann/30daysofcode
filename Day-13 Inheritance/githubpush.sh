307f5f0b388850dbbfa0fed04935014275b6d019
Mustafa <mkafridi073@gmail.com>
2022-08-03 19:54:15 +0500

    All done for Day-13

---
+++ Day-13 Inheritance/githubpush.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Shell script to automate the code pushing to github.
+while :
+do
+# git commands to push the code to github
+    git add .
+    git commit -a -m "All done for Day-13"
+    git push
+# Delay of 20 seconds for every push to github.
+    sleep 20
+done
