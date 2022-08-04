9926422b5526d1705fb52b0cc59618a292c663a6
Mustafa <mkafridi073@gmail.com>
2022-08-04 11:01:58 +0500

    All done for Day-9

--- Day-9 Recursion/Recursion.java
+++ Day-9 Recursion/Recursion.java
@@ -14,7 +14,6 @@ class Result {
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER n as parameter.
      */
-
     public static int factorial(int n) {
     // Write your code here
     if(n<=1)
