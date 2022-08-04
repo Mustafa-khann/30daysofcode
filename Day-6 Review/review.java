71eb89f252526c30bad83a04cac3f321fa4b80d9
Mustafa <mkafridi073@gmail.com>
2022-08-04 11:00:23 +0500

    All done for Day-6

--- Day-6 Review/review.java
+++ Day-6 Review/review.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();sc.nextLine();
