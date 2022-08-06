3927e93c04ff523fcb9b71826c65195ff3136c3c
Mustafa <mkafridi073@gmail.com>
2022-08-06 14:22:36 +0500

    All done for Day-15

--- Day-15 LinkedList/linkedList.cpp
+++ Day-15 LinkedList/linkedList.cpp
@@ -1,36 +1,45 @@
-//
-// Created by mustafa on 8/6/22.
-//
-
 #include <iostream>
+#include <cstddef>
 using namespace std;
-
-class Node{
+class Node
+{
 public:
-    int value;
+    int data;
     Node *next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
 };
+class Solution{
+public:
 
+    Node* insert(Node *head,int data)
+    {
+        //Complete this method
+
+    }
+
+    void display(Node *head)
+    {
+        Node *start=head;
+        while(start)
+        {
+            cout<<start->data<<" ";
+            start=start->next;
+        }
+    }
+};
 int main()
 {
-    Node* head = new Node();
-    Node* first = new Node();
-    Node* second = new Node();
-    Node* third = new Node();
-
-    head->next = first;
-    first->value= 25;
-    first->next = second;
-    second->value = 5;
-    second->next = third;
-    third->value = 30;
-    third->next = nullptr;
-
-    Node* current = head;
-    while(current->next)
-    {
-        cout<<current->value<<"\n";
-        current = current->next;
+    Node* head=NULL;
+    Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
     }
-    return 0;
+    mylist.display(head);
+
 }
