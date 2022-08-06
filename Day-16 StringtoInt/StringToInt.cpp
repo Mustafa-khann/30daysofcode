95fd49a98d5a3e71efb7ed4de4580f7e40bbb746
Mustafa <mkafridi073@gmail.com>
2022-08-06 16:17:33 +0500

    All done for Day-15

--- Day-16 StringtoInt/StringToInt.cpp
+++ Day-16 StringtoInt/StringToInt.cpp
@@ -1,9 +1,8 @@
 //
 // Created by mustafa on 8/6/22.
 //
-
 #include <bits/stdc++.h>
-
+#include <string>
 using namespace std;
 
 
@@ -12,5 +11,13 @@ int main()
 {
     string S;
     getline(cin, S);
+  try{
+    int i = stoi(S);
+    cout<<i;
+    }
+  catch(exception)
+  {
+    cout<<"Bad String"<<endl;
+  }
     return 0;
 }
